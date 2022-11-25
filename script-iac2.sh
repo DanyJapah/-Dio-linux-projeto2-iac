f542d594e266914a5fefc3cd58b2fc7b56f18847
DanyJapah <danyrodriguesti@gmail.com>
2022-11-25 12:36:46 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Atualizando o Servidor..."
+apt-get update 
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-projeto2-iac/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-projeto2-iac-main
+cp -R * /var/www/html/
+
